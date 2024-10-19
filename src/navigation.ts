23fc8a2c01cd92dd85195a2033b121ef87a815b4
minghao <howt51@gmail.com>
2024-10-19 16:41:35 +0800

    Fix formatting

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,4 +1,4 @@
-import { getPermalink,  getAsset } from './utils/permalinks';
+import { getPermalink, getAsset } from './utils/permalinks';
 // getBlogPermalink,
 
 export const headerData = {
@@ -18,7 +18,7 @@ export const headerData = {
     {
       text: 'Personal',
       href: getPermalink('/homes/personal'),
-    }
+    },
   ],
   // links: [
   //   {
@@ -200,5 +200,5 @@ export const footerData = {
     Made using <a class="text-blue-600 underline dark:text-muted" href="https://astro.build/"> Astro</a>
   `,
   // <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    // Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  // Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
 };
