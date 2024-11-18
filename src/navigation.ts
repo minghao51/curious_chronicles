df0d99a30fe8e6f1c69c3e9c508f1a5420f6e587
howt51@gmail.com <howt51@gmail.com>
2024-11-18 22:21:10 +0800

    update base for src/assets

--- src/navigation.ts
+++ src/navigation.ts
@@ -197,7 +197,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/minghao51/curious_chronicles' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/src/assets/favicons/favicon.ico" alt="onWidget logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="src/assets/favicons/favicon.ico" alt="onWidget logo" loading="lazy"></img>
     Made using <a class="text-blue-600 underline dark:text-muted" href="https://astro.build/"> Astro</a>
   `,
   // <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
